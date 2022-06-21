0d5d245789a0195c78f62116ebdecbd4d72ecfb3
mbolonzi-design <>
2022-06-21 16:48:11 +0000

    assignment completed

---
+++ assignment-1/assets/main.js
@@ -0,0 +1,7 @@
+document.getElementById('intro').onclick = function(){
+    greetings()
+}
+
+function greetings(){
+    document.getElementById('intro').innerHTML = "Hello Mbolonzi, welcome to the bootcamp"
+}
